56ae3fea7ee8880041e710b59ed763be80de25ad
ekoroto <evkoroto@gmail.com>
2021-05-17 11:14:38 +0300

    updated in accordance with the comments

--- file_handler.py
+++ file_handler.py
@@ -1,9 +1,12 @@
 from abc import ABC, abstractmethod
-from dict2xml import dict2xml
+from argparse import ArgumentParser
 from json import dumps, loads
 from os import getcwd, mkdir
 from os.path import exists
 from sys import exit
+
+from dict2xml import dict2xml
+
 from my_exception import MyException
 
 
@@ -40,8 +43,16 @@ class FileHandler:
         Handles file processing
     """
 
-    def __init__(self, exporter):
-        self.exporter = exporter
+    def __init__(self):
+        self.args = ArgsHandler()
+        self.students = self.read(self.args.get_students_path())
+        self.rooms = self.read(self.args.get_rooms_path())
+        self.exporter = self.args.get_exporter()
+
+    def merge(self):
+        merged_data = self.rooms
+        self._assign_students(merged_data)
+        return merged_data
 
     def read(self, file_path):
         with open(file_path, 'r') as f:
@@ -49,7 +60,8 @@ class FileHandler:
             data = self._deserialize_json(data)
             return data
 
-    def write(self, merged_data):
+    def write(self):
+        merged_data = self.merge()
         export_data = self.exporter().export(merged_data)
         folder_path = getcwd() + '/output_data/'
         file_path = folder_path + 'output' + self.exporter.extension
@@ -57,6 +69,16 @@ class FileHandler:
             mkdir(folder_path)
         with open(file_path, 'w') as f:
             f.write(export_data)
+
+    def _assign_students(self, rooms_list):
+        """
+            Loop at students list and assign them to corresponding rooms.
+        """
+        for student_dict in self.students:
+            student = student_dict.copy()
+            student.pop('room')
+            rooms_list[student_dict.get('room')].setdefault('students', []).append(student)
+        return rooms_list
 
     @staticmethod
     def _deserialize_json(data):
@@ -68,26 +90,38 @@ class ArgsHandler:
         Validates input arguments
     """
 
-    _args_quantity = 4
     _formats = ('xml', 'json')
 
-    def __init__(self, args):
-        self.args = args
+    def __init__(self):
+        self.args = self.parse_args()
+        self.validate_args()
 
     def get_exporter(self):
-        if self.args[3] == 'xml':
+        if self.args.format == 'xml':
             return XMLExporter
         return JSONExporter
 
+    def get_rooms_path(self):
+        return self.args.rooms_path
+
+    def get_students_path(self):
+        return self.args.students_path
+
+    @staticmethod
+    def parse_args():
+        parser = ArgumentParser()
+        parser.add_argument('students_path', help='Path to file with students.')
+        parser.add_argument('rooms_path', help='Path to file with rooms.')
+        parser.add_argument('format', help='Format of export.')
+        return parser.parse_args()
+
     def validate_args(self):
         try:
-            if len(self.args) != self._args_quantity:
-                raise MyException('Incorrect number of input arguments, should be 3.')
-            elif not exists(self.args[1]):
+            if not exists(self.args.students_path):
                 raise MyException('Incorrect path to first file.')
-            elif not exists(self.args[2]):
+            elif not exists(self.args.rooms_path):
                 raise MyException('Incorrect path to second file.')
-            elif self.args[3] not in self._formats:
+            elif self.args.format not in self._formats:
                 raise MyException('Incorrect format of export.')
 
         except MyException as err:
--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-import file_handler
-from merge_files import DictsMerger
-from sys import argv
+from file_handler import FileHandler
 
 
 if __name__ == '__main__':
-
-    args = file_handler.ArgsHandler(argv)
-    args.validate_args()
-
-    handler = file_handler.FileHandler(args.get_exporter())
-    students = handler.read(argv[1])
-    rooms = handler.read(argv[2])
-
-    merged_data = DictsMerger(rooms, students).merge()
-    handler.write(merged_data)
+    handler = FileHandler()
+    handler.write()
